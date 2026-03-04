b5e782f024ea85c1f04ce9a9752331f3228f9a76
Elizabeth Sterling <sterling.elizabeth08@gmail.com>
2026-03-04 10:58:07 -0500

    Got the arrow design finished and made it so when you click on it, you go to the mai activity. Still need to figure out how to start on the GetStarted page

--- app/src/main/java/edu/bpi/hiitmasterapp/GetStarted.java
+++ app/src/main/java/edu/bpi/hiitmasterapp/GetStarted.java
@@ -1,6 +1,10 @@
 package edu.bpi.hiitmasterapp;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class GetStarted extends AppCompatActivity {
+
+    ImageView Arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_get_started);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Arrow = (ImageView)findViewById(R.id.Arrow);
+
+        Arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GetStarted.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
